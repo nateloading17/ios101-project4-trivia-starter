fbffe0b22eaccdcedc3a27f4bf6bad90c3aaec5b
nateloading17 <nathanmilobaez@gmail.com>
2024-03-13 18:14:24 -0400

    Updated

---
+++ Trivia/TriviaAPI.swift
@@ -0,0 +1,8 @@
+//
+//  TriviaAPI.swift
+//  Trivia
+//
+//  Created by Nathaniel Baez on 3/12/24.
+//
+
+import Foundation
---
+++ Trivia/TriviaQuestionRetrieval.swift
@@ -0,0 +1,8 @@
+//
+//  TriviaQuestionRetrieval.swift
+//  Trivia
+//
+//  Created by Nathaniel Baez on 3/13/24.
+//
+
+import Foundation
