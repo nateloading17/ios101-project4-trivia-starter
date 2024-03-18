5352396b45a98a626e189ddd762fba3ea9a14cda
nateloading17 <72894219+nateloading17@users.noreply.github.com>
2024-03-18 10:26:54 -0400

    Update TriviaQuestion.swift

--- Trivia/TriviaQuestion.swift
+++ Trivia/TriviaQuestion.swift
@@ -7,9 +7,16 @@
 
 import Foundation
 
-struct TriviaQuestion {
+struct TriviaQuestion: Decodable {
   let category: String
   let question: String
   let correctAnswer: String
   let incorrectAnswers: [String]
+    
+    private enum CodingKeys: String, CodingKey{
+        case category
+        case question
+        case correctAnswer = "correct_answer"
+        case incorrectAnswers = "incorrect_answers"
+    }
 }
